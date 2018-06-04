9e85c5de25cd71229f5c57fe8a4c5663d9edda18
liuqiang <liuqiang200832@hotmail.com>
2018-06-04 18:23:19 +0800

    ddd

--- robot-nike/src/main/java/com/lq/robot/TestMain.java
+++ robot-nike/src/main/java/com/lq/robot/TestMain.java
@@ -4,7 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 //对接口进行测试
 public class TestMain {
-    private String url = "https://unite.nike.com/login?appVersion=431&experienceVersion=360&uxid=com.nike.commerce.nikedotcom.web&locale=zh_CN&backendEnvironment=identity&browser=Google%20Inc.&os=undefined&mobile=false&native=false&visit=1&visitor=542767df-ea1a-4f8d-8c36-b72d58c2fbb2";
+    //private String url = "https://unite.nike.com/appInitialization?appVersion=435&experienceVersion=361&uxid=com.nike.commerce.nikedotcom.web&locale=zh_CN&backendEnvironment=identity&browser=Google%20Inc.&os=undefined&mobile=false&native=false&visit=&visitor=&clientId=HlHa2Cje3ctlaOqnxvgZXNaAs7T9nAuH&status=success&uxId=com.nike.commerce.nikedotcom.web&isAndroid=false&isIOS=false&isMobile=false&isNative=false&timeElapsed=632";
+    //private String url = "https://unite.nike.com/login?appVersion=435&experienceVersion=361&uxid=com.nike.commerce.nikedotcom.web&locale=zh_CN&backendEnvironment=identity&browser=Google%20Inc.&os=undefined&mobile=false&native=false&visit=2&visitor=9be0d89a-6cd2-455a-a98b-8ecb4d593ec8";
+    //private String url = "https://unite.nike.com/login?appVersion=435&experienceVersion=361&uxid=com.nike.commerce.nikedotcom.web&locale=zh_CN&backendEnvironment=identity&browser=Google%20Inc.&os=undefined&mobile=false&native=false&visit=1&visitor=878d9bb9-ea87-424f-87db-59a66593b046";
+    private String url = "https://www.nike.com/cn/zh_cn/";
     private String charset = "utf-8";
     private HttpClientUtil httpClientUtil = null;
 
@@ -13,28 +16,18 @@ public class TestMain {
     }
 
     public void test(){
-        //{"username":"1@11.com",
-        // "password":"1",
-        // "client_id":"HlHa2Cje3ctlaOqnxvgZXNaAs7T9nAuH",
-        // "ux_id":"com.nike.commerce.nikedotcom.web",
-        // "grant_type":"password"}
+
         String httpOrgCreateTest = url ;//+ "httpOrg/create";
         Map<String,String> createMap = new HashMap<String,String>();
-        createMap.put("username","tianshi139803@126.com");
+        /*createMap.put("username","tianshi139803@126.com");
         createMap.put("password","liu123456L");
         createMap.put("client_id","HlHa2Cje3ctlaOqnxvgZXNaAs7T9nAuH");
         createMap.put("ux_id","com.nike.commerce.nikedotcom.web");
-        createMap.put("grant_type","password");
-        String httpOrgCreateTestRtn = httpClientUtil.doPost(httpOrgCreateTest,createMap,charset);
-        System.out.println("result:"+httpOrgCreateTestRtn);
+        createMap.put("grant_type","password");*/
+        httpClientUtil.request();
+        //System.out.println("result:"+httpOrgCreateTestRtn);
     }
 
-    public void openIndex(){
-        String httpOrgCreateTest = "https://www.nike.com/cn/zh_cn/";
-        Map<String,String> createMap = new HashMap<String,String>();
-        String httpOrgCreateTestRtn = httpClientUtil.doPost(httpOrgCreateTest,createMap,charset);
-        System.out.println("result:"+httpOrgCreateTestRtn);
-    }
     public static void main(String[] args){
         TestMain main = new TestMain();
         main.test();
