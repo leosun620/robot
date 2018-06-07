37132fd25134f771f72a480befe26a85fa8c88ad
lyqylq <liuqiang200832@gmail.com>
2018-06-08 00:44:45 +0800

    dd

--- robot-nike/src/main/java/com/lq/spider/SpiderDemo.java
+++ robot-nike/src/main/java/com/lq/spider/SpiderDemo.java
@@ -9,6 +9,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
@@ -27,33 +32,39 @@ public class SpiderDemo implements PageProcessor {
     @Override
     public void process(Page page) {
         //获取用户的id
-        page.putField("", page.getHtml().xpath("//div[@class='member_info_r yh']/h4/span/text()"));
-
+        //page.putField("", page.getHtml().xpath("//div[@class='page-index'"));
+        page.addTargetRequests(page.getHtml().links().regex("https://www.nike.com/cn/t.*").all());
         //获取用户的详细信息
-        List<String> information = page.getHtml().xpath("//ul[@class='member_info_list fn-clear']//li/div[@class='fl pr']/em/text()").all();
-        page.putField("information = ", information);
-
+        //List<String> information = page.getHtml().xpath("//div[@class='page-index'").all();
+        //page.putField("information = ", information);
+        page.putField("production",page.getHtml().xpath("//h2[@class='fs16-sm pb1-sm']"));
+        page.putField("productionE",page.getHtml().xpath("//h1[@class='fs26-sm fs28-lg']"));
+        page.putField("price",page.getHtml().xpath("//div[@class='text-color-black']"));
+        page.putField("color",page.getHtml().xpath("//img[@class='bg-medium-grey']"));
     }
 
     //使用 selenium 来模拟用户的登录获取cookie信息
     public void login()
     {
-        WebDriver driver = new ChromeDriver();
+        //WebDriver driver = new ChromeDriver();
+        WebDriver driver = getPhantomJSDriver();
         driver.get("https://www.nike.com/cn/zh_cn/");
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("login-text")));
 
         driver.findElement(By.className("login-text")).click();
 
         //在******中填你的用户名
-        WebElement element = driver.findElement(By.xpath("//input@[data-componentname=\"emailAddress\"]"));
-        System.out.println(element.toString());
-        element.sendKeys("*******");
+        WebElement userNameElement = driver.findElement(By.xpath("//input[@data-componentname='emailAddress']"));
+        System.out.println(userNameElement.toString());
+        userNameElement.sendKeys("tianshi139803@126.com");
 
-        driver.findElement(By.id("login_password")).clear();
+        WebElement passwordElement = driver.findElement(By.xpath("//input[@data-componentname='password']"));
         //在*******填你密码
-        driver.findElement(By.id("login_password")).sendKeys("*******");
+        passwordElement.sendKeys("liu123456L");
 
         //模拟点击登录按钮
-        driver.findElement(By.id("login_btn")).click();
+        driver.findElement(By.xpath("//input[@value='登录']")).click();
 
         //获取cookie信息
         cookies = driver.manage().getCookies();
@@ -67,16 +78,35 @@ public class SpiderDemo implements PageProcessor {
             site.addCookie(cookie.getName().toString(),cookie.getValue().toString());
         }
 
-        return site.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1");
+        return site.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36");
     }
+    public static PhantomJSDriver getPhantomJSDriver(){
+        //设置必要参数
+        DesiredCapabilities dcaps = new DesiredCapabilities();
+        //ssl证书支持
+        dcaps.setCapability("acceptSslCerts", true);
+        //截屏支持
+        dcaps.setCapability("takesScreenshot", false);
+        //css搜索支持
+        dcaps.setCapability("cssSelectorsEnabled", true);
+        //js支持
+        dcaps.setJavascriptEnabled(true);
+        //设置参数
+        dcaps.setCapability("phantomjs.page.settings.userAgent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36");
+        dcaps.setCapability("phantomjs.page.customHeaders.User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36");
+        //驱动支持
+        dcaps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,"E:\\git\\phantomjs-2.1.1-windows\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe");
 
+        PhantomJSDriver driver = new PhantomJSDriver(dcaps);
+        return  driver;
+    }
     public static void main(String[] args){
         SpiderDemo miai = new SpiderDemo();
-        System.setProperty("webdriver.chrome.driver","C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","E:\\git\\chromedriver_win32\\chromedriver.exe");
         //调用selenium，进行模拟登录
         miai.login();
         Spider.create(miai)
-                .addUrl("http://www.jiayuan.com/164830633")
+                .addUrl("https://www.nike.com/cn/t/air-max-270-%E7%94%B7%E5%AD%90%E8%BF%90%E5%8A%A8%E9%9E%8B-v0oVPL/AH8050-002")
                 .run();
     }
 }
