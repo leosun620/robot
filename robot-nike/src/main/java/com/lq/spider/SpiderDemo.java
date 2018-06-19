755b16ccc9c35015bc7a9855581aa648267c7737
liuqiang <liuqiang200832@hotmail.com>
2018-06-19 13:07:38 +0800

    ddd

--- robot-nike/src/main/java/com/lq/spider/SpiderDemo.java
+++ robot-nike/src/main/java/com/lq/spider/SpiderDemo.java
@@ -42,6 +42,7 @@ public class SpiderDemo implements PageProcessor {
         page.putField("productionE",page.getHtml().xpath("//h1[@class='fs26-sm fs28-lg']"));
         page.putField("price",page.getHtml().xpath("//div[@class='text-color-black']"));
         page.putField("color",page.getHtml().xpath("//img[@class='bg-medium-grey']"));
+        page.putField("additem",page.getHtml().xpath("//div[@class='buying-tools-container']"));
     }
 
     private void takesScreenshot(WebDriver driver,String name){
@@ -57,8 +58,8 @@ public class SpiderDemo implements PageProcessor {
     public void login()
     {
 
-        WebDriver driver = getPhantomJSDriver();
-        //WebDriver driver = getChromeDriver();
+        //WebDriver driver = getPhantomJSDriver();
+        WebDriver driver = getChromeDriver();
         driver.get("https://www.nike.com/cn/zh_cn/");
         driver.manage().window().maximize();
         takesScreenshot(driver,"index.png");
@@ -68,26 +69,20 @@ public class SpiderDemo implements PageProcessor {
         webElement.click();
         takesScreenshot(driver,"login.png");
 
-        List<WebElement> elements = driver.findElements(By.tagName("a"));
-        for(int i=0; i<elements.size();i++) {
-            WebElement element = elements.get(i);
-            System.out.println(i+" "+element.getText());
-            if (element != null && element.getText().contains("使用电子邮件登录")) {
-                element.click();
-                break;
-            }
-        }
+        WebElement element = driver.findElement(By.xpath("//a[contains(text(),'使用电子邮件登录')]"));
+        element.click();
 
         try{
             input(wait,driver);
         }catch (Exception e){
             e.printStackTrace();
         }
-
+        takesScreenshot(driver,"login-ed.png");
 
         //获取cookie信息
         cookies = driver.manage().getCookies();
         driver.close();
+
     }
 
     private  void input(WebDriverWait wait,WebDriver driver){
@@ -111,7 +106,7 @@ public class SpiderDemo implements PageProcessor {
         for (Cookie cookie : cookies) {
             site.addCookie(cookie.getName().toString(),cookie.getValue().toString());
         }
-
+        site.addHeader("content-type","application/json");
         return site.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36");
     }
 
@@ -157,7 +152,7 @@ public class SpiderDemo implements PageProcessor {
         //调用selenium，进行模拟登录
         miai.login();
         Spider.create(miai)
-                .addUrl("https://www.nike.com/cn/t/air-max-270-%E7%94%B7%E5%AD%90%E8%BF%90%E5%8A%A8%E9%9E%8B-v0oVPL/AH8050-002")
+                .addUrl("https://www.nike.com/cn/launch/t/air-jordan-2-retro-melo/")
                 .run();
     }
 }
