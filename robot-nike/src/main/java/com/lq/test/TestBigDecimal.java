2ad949fe2ad0da595a9559b0b3603332f3a90e0b
liuqiang <liuqiang200832@hotmail.com>
2018-09-12 10:15:02 +0800

    ddd

---
+++ robot-nike/src/main/java/com/lq/test/TestBigDecimal.java
@@ -0,0 +1,13 @@
+package com.lq.test;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by liuqiang on 2018/7/24.
+ */
+public class TestBigDecimal {
+    public static void main(String[] args){
+        BigDecimal bigDecimal = new BigDecimal(1.1);
+        System.out.println(bigDecimal+"");
+    }
+}
